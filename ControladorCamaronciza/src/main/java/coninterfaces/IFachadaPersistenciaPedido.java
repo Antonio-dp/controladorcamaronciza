89a9238965bfac0aea230fe208e6ef35a3bfc540
Antonio-dp <antonio.delpardo229161@potros.itson.edu.mx>
2023-03-16 10:10:57 -0700

    Update IFachadaPersistenciaPedido.java

--- ControladorCamaronciza/src/main/java/coninterfaces/IFachadaPersistenciaPedido.java
+++ ControladorCamaronciza/src/main/java/coninterfaces/IFachadaPersistenciaPedido.java
@@ -6,6 +6,6 @@ import java.util.List;
 
 public interface IFachadaPersistenciaPedido {
     public void crearPedido(Pedido pedido);
-    public void editarPedido(Pedido pedido);
+    public void consultarPedido(Pedido pedido);
     public void eliminarPedido(Pedido pedido);
 }
